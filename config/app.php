c646a92856664c92fa064c25e9cc8899ffc3e052
carterheron <carterheron@gmail.com>
2021-11-19 10:30:14 -0500

    Updated files for headless mode

--- config/app.php
+++ config/app.php
@@ -5,65 +5,24 @@
  * Edit this file at your own risk!
  *
  * The array returned by this file will get merged with
- * vendor/craftcms/cms/src/config/app/main.php and [web|console].php, when
+ * vendor/craftcms/cms/src/config/app.php and app.[web|console].php, when
  * Craft's bootstrap script is defining the configuration for the entire
  * application.
  *
  * You can define custom modules and system components, and even override the
  * built-in system components.
+ *
+ * If you want to modify the application config for *only* web requests or
+ * *only* console requests, create an app.web.php or app.console.php file in
+ * your config/ folder, alongside this one.
  */
 
+use craft\helpers\App;
+
 return [
-
-    // All environments
-    '*' => [
-
-        'components' => [
-            // Default to database 0, so PHP sessions are in a separate database
-            'redis' => [
-                'class' => yii\redis\Connection::class,
-                'hostname' => getenv('REDIS_HOST'),
-                'port' => getenv('REDIS_PORT'),
-                'database' => getenv('REDIS_DB'),
-            ],
-            'cache' => [
-                // Use database 1 for live production
-                'class' => yii\redis\Cache::class,
-                'redis' => [
-                    'hostname' => getenv('REDIS_HOST'),
-                    'port' => getenv('REDIS_PORT'),
-                    'database' => getenv('REDIS_DB'),
-                ],
-            ],
-            'session' => function () {
-                $stateKeyPrefix = md5('Craft.' . craft\web\Session::class . '.' . Craft::$app->id);
-                /** @var yii\redis\Session $session */
-                $session = Craft::createObject([
-                    'class' => yii\redis\Session::class,
-                    'flashParam' => $stateKeyPrefix . '__flash',
-                    'name' => Craft::$app->getConfig()->getGeneral()->phpSessionName,
-                    'cookieParams' => Craft::cookieConfig(),
-                ]);
-                $session->attachBehaviors([craft\behaviors\SessionBehavior::class]);
-                return $session;
-            },
-        ],
+    'id' => App::env('APP_ID') ?: 'CraftCMS',
+    'modules' => [
+        'my-module' => \modules\Module::class,
     ],
-
-    // Live (production) environment
-    'production' => [
-    ],
-
-    // Staging (pre-production) environment
-    'staging' => [
-    ],
-
-    // Local (development) environment
-    'dev' => [
-        'components' => [
-            'deprecator' => [
-                'throwExceptions' => true,
-            ],
-        ]
-    ],
+    //'bootstrap' => ['my-module'],
 ];
--- config/general.php
+++ config/general.php
@@ -7,64 +7,28 @@
  *
  * @see craft\config\GeneralConfig
  */
+
 return [
     '*' => [
-        // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 0,
-
-        // Enable CSRF Protection (recommended)
-        'enableCsrfProtection' => true,
-
-
-        // Whether "index.php" should be visible in URLs
         'omitScriptNameInUrls' => true,
-
-        'imageDriver' => 'imagick',
-
-        // Gif sanitation tends to blow up servers due to high memory consumption
-        'transformGifs' => false,
-
-        // Control Panel trigger word
+        "useEmailAsUsername" => true,
         'cpTrigger' => 'admin',
-
-        // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
-
-        'maxUploadFileSize' => 33554432 * 7,
-
         'useEmailAsUsername' => true,
-
-        'extraAllowedFileExtensions' => 'json,rvt,dwg,dxf,iges',
-
-        'baseCpUrl' => getenv('PRIMARY_SITE_URL'),
-
-        'devMode' => false,
-
-        'runQueueAutomatically' => true,
-
-        'cacheDuration' => 0,
-
-        'enableTemplateCaching' => true,
-
-        'errorTemplatePrefix' => '_errors',
-
-        'useProjectConfigFile' => false,
-        'allowAdminChanges' => true,
-        'aliases' => [
-            '@assetBaseUrl' => getenv('ASSET_BASE_URL'),
-            '@assetBasePath' => getenv('ASSET_BASE_PATH'),
-            '@dist' => getenv('DIST_BASE_URL') ?? '/',
-            '@src' => CRAFT_BASE_PATH . '/src' ?? '/src',
-            '@web' => getenv('PRIMARY_SITE_URL')
-        ]
+        'baseCpUrl' => getenv('DEFAULT_SITE_URL'),
+        'disallowRobots' => false,
+        'headlessMode' => true,
+        'enableGraphqlCaching' => true
     ],
     'staging' => [
         'devMode' => true,
-        'allowAdminChanges' => true,
+        'disallowRobots' => true,
     ],
     'dev' => [
         'allowAdminChanges' => true,
         'enableTemplateCaching' => false,
         'devMode' => true,
+        'disallowRobots' => true,
     ],
 ];
